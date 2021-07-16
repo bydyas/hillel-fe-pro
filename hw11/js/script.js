23f09e3dee8ccb1fd58a0668029df9de2367ad87
bydyas <bogdan.shkaran@gmail.com>
2021-07-16 11:50:24 +0300

    init

---
+++ hw11/js/script.js
@@ -0,0 +1,38 @@
+const inputNewItem = document.querySelector('#inputNewItem');
+const btnAddNewItem = document.querySelector('#btnAddNewItem');
+const toDoListContainer = document.querySelector('#toDoListContainer');
+let counter = 0;
+
+function createNewItem(input, parent) {
+    const itemsLimit = 10;
+    const lenLimit = 35;
+    let str = input.value;
+
+    if (str.replace(/\s/g, '').length && str !== '' && str.length < lenLimit && counter < itemsLimit) {
+
+        let newItem = document.createElement('li');
+        newItem.classList.add('new-item');
+        newItem.innerHTML = '<span>' + str + '</span>';
+        newItem.onclick = () => {
+            newItem.classList.toggle('is-completed');
+        };
+
+        let binBtn = document.createElement('span');
+        binBtn.classList.add('close-x');
+        binBtn.onclick = () => {
+            binBtn.parentNode.remove();
+            counter--;
+        };
+
+        newItem.append(binBtn);
+        parent.append(newItem);
+        counter++;
+    }
+
+    input.value = '';
+}
+
+btnAddNewItem.onclick = (e) => {
+    e.preventDefault();
+    createNewItem(inputNewItem, toDoListContainer);
+};
