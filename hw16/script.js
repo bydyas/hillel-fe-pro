0126fc7bfb3ae4fa968409e535ab6bf04fb4f695
bydyas <bogdan.shkaran@gmail.com>
2021-08-04 16:03:39 +0300

    released

--- hw16/script.js
+++ hw16/script.js
@@ -1,9 +1,53 @@
+// Task #1
+const personJohn = {
+  name: "John",
+  sayHello: function() {
+    console.log(`Hello, I'm ${this.name}`);
+  }
+}
+
+const sysAdmin = {
+  name: "Bob",
+  __proto__: personJohn
+}
+
+const clientNatalia = {
+  name: "Natalia",
+  __proto__: personJohn
+}
+//console.log(sysAdmin.sayHello());
+
+// Task #2
+function Person(name) {
+  this.name = name;
+  this.sayHello = function() {
+    console.log(`Hello, I'm ${this.name}`);
+  }
+}
+
+function Employee(name) {
+  this.name = name;
+}
+
+function Client(name) {
+  this.name = name;
+}
+
+Employee.prototype = new Person();
+Client.prototype = new Person();
+
+const personJohn = new Person("John");
+const sysAdmin = new Employee("Bob");
+const clientNatalia = new Client("Natalia");
+//console.log(Employee.sayHello());
+
+// Task #3
 function Student(name, marks) {
   this.name = name;
   this.marks = marks;
   this.averageMark = function () {
-    let totalMark = this.marks.reduce((sum, num) => sum + num);
-    let averageMark = totalMark / this.marks.length;
+    let totalMark = this.marks.reduce((sum, mark) => sum + mark);
+    let averageMark = Math.ceil((totalMark / this.marks.length)*100)/100;
     return averageMark;
   };
 }
@@ -12,5 +56,4 @@ const students = [
   new Student("Student 1", [10, 9, 8, 0, 10]),
   new Student("Student 12", [10, 0, 8, 0, 3, 4]),
 ];
-
-console.log(students);
+//console.log(students[1].averageMark());
