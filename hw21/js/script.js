9dbf8b088ed48ab2d7bf46967cb4e8c005f411c9
bydyas <bogdan.shkaran@gmail.com>
2021-08-20 18:25:34 +0300

    added func sendData

--- hw21/js/script.js
+++ hw21/js/script.js
@@ -1,12 +1,18 @@
 const TODOS_TEMPLATE = document.querySelector("#todosTemplate").innerHTML;
 const TODOS_CONTAINER = document.querySelector("#todosContainer");
 const TODOS_AMOUNT_TO_SHOW = 100; // configure by yourself
+const BTN_ADD_TODO = document.querySelector("#btnAddNewItem");
+
 
 window.addEventListener('DOMContentLoaded', getData, false);
+
 TODOS_CONTAINER.addEventListener('click', (e) => {
     changeStatus(e);
     deleteTodo(e);
 }, false);
+
+BTN_ADD_TODO.addEventListener('click', sendData, false);
+
 
 function getData() {
     fetch("https://jsonplaceholder.typicode.com/todos")
@@ -37,12 +43,32 @@ function changeStatus(e) {
     if (e.target.tagName === 'LI') {
         e.target.classList.toggle('is-completed');
     }
+    // send to server
 }
 
 function deleteTodo(e) {
     if (e.target.tagName === 'SPAN') {
         e.target.parentNode.remove();
     }
+    // send to server
+}
+
+// fake request
+function sendData() {
+    fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: 'POST',
+        body: JSON.stringify({
+            userId: 1,
+            id: 3,
+            title: "fugiat veniam minus",
+            completed: false
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .catch(error => console.log(error))
+        .finally(getData());
 }
 
 // To animate the pseudo of #form by focusing input el;
